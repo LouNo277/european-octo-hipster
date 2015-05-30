23dcc5d62fced537afb711e7f15b9a8ebdd618a4
Louisa Noffke <Louisa.Noffke@gmail.com>
2015-05-30 19:14:10 +0100

    add constants file with initial set of values

---
+++ SCK/constants.py
@@ -0,0 +1,9 @@
+# TODO: Content of file to be reduced to nothing.
+# TODO: Constants should be provided as user defined parameters / arguments.
+
+import os
+
+WORKBOOK_DIR = r'G:\\Analytical Sciences\\PMAS3\\Structural_Chemistry\\Structure_Checks\\GABA_HTS\\'
+WORKBOOK_FILE = os.path.join(WORKBOOK_DIR, '20150520_GABA-05.xlsm')
+WORKBOOK_SHEET = 'Plate'
+TARGETS_TEMPLATE = r'G:\\Analytical Sciences\PMAS3\\Structural_Chemistry\\Structure_Checks\\_in-progress\\_scripts\\ACD_ITA\\TARGET_TEMPLATE.xml'
