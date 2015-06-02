4bef9653c37c438bcdba53d49cbe22b477b09306
Louisa Noffke <Louisa.Noffke@gmail.com>
2015-06-02 10:15:51 +0100

    update WORKBOOK_DIR and WORKBOOK_FILE in constants.

--- SCK/constants.py
+++ SCK/constants.py
@@ -3,8 +3,8 @@
 
 import os
 
-WORKBOOK_DIR = r'G:\\Analytical Sciences\\PMAS3\\Structural_Chemistry\\Structure_Checks\\GABA_HTS\\'
-WORKBOOK_FILE = os.path.join(WORKBOOK_DIR, '20150520_GABA-05.xlsm')
+WORKBOOK_DIR = r'G:\\Analytical Sciences\\PMAS3\\Structural_Chemistry\\Structure_Checks\\_in-progress\\'
+WORKBOOK_FILE = os.path.join(WORKBOOK_DIR, '201506_RemoteAnalyzerTemplate.xlsm')
 WORKBOOK_SHEET = 'Plate'
 
 TARGETS_TEMPLATE = r'G:\\Analytical Sciences\PMAS3\\Structural_Chemistry\\Structure_Checks\\_in-progress\\_scripts\\ACD_ITA\\TARGET_TEMPLATE.xml'
