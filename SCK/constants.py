c94cd210c257536e1327f9b3d8d563e46602bbd7
Louisa Noffke <Louisa.Noffke@gmail.com>
2015-06-05 15:08:48 +0100

    add 'get_water' constants to constants.py

--- SCK/constants.py
+++ SCK/constants.py
@@ -7,7 +7,14 @@ WORKBOOK_DIR = r'G:\\Analytical Sciences\\PMAS3\\Structural_Chemistry\\Structure
 WORKBOOK_FILE = os.path.join(WORKBOOK_DIR, '20150602_MOA1.xlsm')
 WORKBOOK_SHEET = 'Plate'
 
+# xml_factory
 TARGETS_TEMPLATE = r'G:\\Analytical Sciences\PMAS3\\Structural_Chemistry\\Structure_Checks\\_in-progress\\_scripts\\ACD_ITA\\TARGET_TEMPLATE.xml'
 
+# obtain_hts_results
 RESULTS_FILE = 'INTTARG_RESULT.xls'
 PEAK_LIST = 'PEAKTAB_RESULT.xls'
+
+# get_water
+FILENAME_WATER = 'water.txt'
+FILENAME_DMSO = 'dmso.txt'
+DMSO_CONCENTRATION = 84465
