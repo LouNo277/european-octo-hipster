b0df6ee7d0ac65e64270e744a51d19d0bb42e657
Louisa Noffke <Louisa.Noffke@gmail.com>
2015-06-07 14:30:34 +0100

    TODO added

--- SCK/constants.py
+++ SCK/constants.py
@@ -1,10 +1,11 @@
 # TODO: Content of file to be reduced to nothing.
 # TODO: Constants should be provided as user defined parameters / arguments.
+# TODO: Write a class for the workbook that holds the filename and sheet names and defines the data range for input and output(s)
 
 import os
 
 # main excel workbook which contains the initial set of data
-WORKBOOK_DIR = os.curdir
+WORKBOOK_DIR = os.curdir   # placeholder for file location
 WORKBOOK_FILE = os.path.join(WORKBOOK_DIR, 'default_input.xlsm')
 WORKBOOK_SHEET = 'Plate'
 
